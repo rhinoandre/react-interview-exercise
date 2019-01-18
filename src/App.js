0d6f3b3703323d077f2c2d4e2cd5d2ee5924644b
Rhino <rhinoandre@gmail.com>
2019-01-18 15:18:28 -0200

    Move App.js to src folder

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import FriendListApp from './containers/FriendListApp';
+import * as reducers from './reducers';
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+
+export default function App () {
+  return (
+    <div>
+      <Provider store={store}>
+        <FriendListApp />
+      </Provider>
+    </div>
+  );
 }
-
-export default App;
