55ca5ca591d3e248a9950e37866d64a0aee6a03f
Rhino <rhinoandre@gmail.com>
2019-01-18 15:21:24 -0200

    Improve JSX redability

--- src/components/AddFriendForm/Gender.js
+++ src/components/AddFriendForm/Gender.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Gender.css';
+
+export const FEMALE = 'f'
+export const MALE = 'm'
 
 export default function Gender({onGenderChange, value}) {
   return (
     <div className={styles.gender}>
-      <input id="female" type="radio" name="gender" value="f" onClick={onGenderChange} checked={value === 'f'} />
-      <label htmlFor="female" title="Gender">
+      <input
+        id="female"
+        type="radio"
+        name="gender"
+        value={FEMALE}
+        onChange={onGenderChange}
+        checked={value === FEMALE} />
+      <label role="radio" htmlFor="female" title="Gender">
         <i className="icon-female"></i>
       </label>
-      <input id="male" type="radio" name="gender" value="m" onClick={onGenderChange} checked={value === 'm'} />
-      <label htmlFor="male" title="Gender">
+      <input
+        id="male"
+        type="radio"
+        name="gender"
+        value={MALE}
+        onChange={onGenderChange}
+        checked={value === MALE} />
+      <label role="radio" htmlFor="male" title="Gender">
         <i className="icon-male"></i>
       </label>
     </div>
   );
+}
+
+Gender.propTypes = {
+  onGenderChange: PropTypes.func.isRequired,
+  value: PropTypes.string
 }
